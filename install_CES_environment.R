e532ddd2b287ff0cf7f0c7c16ab44fd8942807b1
khufkens <koen.hufkens@gmail.com>
2022-03-24 11:49:51 +0100

    wrapper function, clean output

--- install_CES_environment.R
+++ install_CES_environment.R
@@ -1,6 +1,22 @@
 # installing required R packages from the main repo
 options(repos=structure(c(CRAN = "http://cran.r-project.org")))
 
+install_package <- function(package){
+  if (grepl("/", package)){
+    # github install
+    status <- try(devtools::install_github("computationales/rsofun", quiet = TRUE))
+  } else {
+    # normal install
+    status <- try(install.packages(package, quiet = TRUE))
+  }
+
+  if(!inherits(status, "try-error")){
+    message(paste0(".... SUCCESS installed: ", package))
+  } else {
+    message(paste0(".... FAILED install: ", package))
+  }
+}
+
 message(
   "
   This routine installs all CES related packages, however
@@ -15,41 +31,87 @@ message(
   )
 
 #---- data wrangling and plotting -----
-# tidyverse environment
-install.packages("tidyverse")
+message("- Installing data science tools")
 
-# patchwork for plotting with ggplot2
-install.packages("patchwork")
+pkgs <- c(
+  "tidyverse",
+  "broom",
+  "multidplyr",
+  "lubridate",
+  "patchwork"
+  )
+
+invisible(lapply(pkgs, install_package))
 
 #---- geospatial libraries -----
-install.packages("raster")
-install.packages("terra")
-install.packages("sf")
-install.packages("rnaturalearth")
-install.packages("rgdal")
-install.packages("rgeos")
-install.packages("MODISTools")
-install.packages("daymetr")
-install.packages("ecmwfr")
+message("- Installing geospatial libraries")
+
+pkgs <- c(
+  "raster",
+  "terra",
+  "sf",
+  "rnaturalearth",
+  "rgdal",
+  "rgeos",
+  "MODISTools",
+  "daymetr",
+  "ecmwfr",
+  "ncdf4",
+  "gdalUtils"
+  )
+
+invisible(lapply(pkgs, install_package))
 
 #---- development ----
-install.packages("devtools")
-install.packages("goodpractice")
-install.packages("pkgdown")
-install.packages("bookdown")
-install.packages("lintr")
+message("- Installing development tools")
+
+pkgs <- c(
+  "utils",
+  "devtools",
+  "goodpractice",
+  "pkgdown",
+  "bookdown",
+  "lintr",
+  "covr",
+  "rmarkdown",
+  "knitr",
+  "testthat",
+  "usethis"
+  )
+
+invisible(lapply(pkgs, install_package))
 
 #---- Optimization ----
-install.packages("BayesianTools")
-install.packages("GenSA")
+message("- Installing optimizers")
+
+pkgs <- c(
+  "BayesianTools",
+  "GenSA"
+  )
+
+invisible(lapply(pkgs, install_package))
+
+#---- machine learning in R ----
+message("- Installing ML tools")
+
+pkgs <- c(
+  "randomForest",
+  "keras",
+  "caret"
+)
+
+invisible(lapply(pkgs, install_package))
 
 #---- ecophys modelling CES ----
-install.packages("hwsdr")
-install.packages("rpmodel")
-install.packages("climate")
-devtools::install_github("computationales/rsofun")
-devtools::install_github("computationales/ingestr")
+message("- Installing CES packages")
 
-#---- deep learning in R ----
-install.packages("keras")
-install.packages("caret")
+pkgs <- c(
+  "hwsdr",
+  "rpmodel",
+  "climate",
+  "computationales/rsofun", # use the github paths, this works!!
+  "computationales/ingestr"
+)
+
+invisible(lapply(pkgs, install_package))
+
